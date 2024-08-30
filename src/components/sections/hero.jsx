4e7e7d7d04d3a2e0466a921933e7c4c77b94af90
Gedeon J. GBEDONOU <beethovengedeon@gmail.com>
2024-08-30 19:18:31 +0100

    Update colors

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import profile from "../../assets/images/about/me.jpg"
-import { RiDownloadLine } from '@remixicon/react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { RiDownloadLine } from '@remixicon/react';
+import { Link } from 'react-router-dom';
+
+const profile = React.lazy(() => import('../../assets/images/about/me.jpg'));
 
 const Hero = () => {
     return (
@@ -19,7 +20,7 @@ const Hero = () => {
                             <h1>I'm Gedeon, an AI & Software Engineer driving innovation in tech.</h1>
                             <h2>Specializing in AI solutions and cutting-edge software development to transform digital landscapes.</h2>
                             <div className="hero-btns">
-                                <Link to="/contact" className="theme-btn">Download CV <i><RiDownloadLine size={16}/></i></Link>
+                                <Link to="https://kloo.me/resumebethogedeon" className="theme-btn">Download CV <i><RiDownloadLine size={16}/></i></Link>
                             </div>
                         </div>
                         {/* <!-- / END HERO DESIGN AREA --> */}
