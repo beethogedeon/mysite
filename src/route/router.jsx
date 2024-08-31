7da203a3bb33a61898610ae3793bac25482d7ad9
Gedeon J. GBEDONOU <beethovengedeon@gmail.com>
2024-08-31 01:48:22 +0100

    Remove blog pages

--- src/route/router.jsx
+++ src/route/router.jsx
@@ -5,11 +5,8 @@ import RootLayout from "../layout/root";
 const Home = React.lazy(() => import('../pages/home'));
 const Contact = React.lazy(() => import('../pages/contact'));
 const About = React.lazy(() => import('../pages/about'));
-const SingleProject = React.lazy(() => import('../pages/single-project'));
 const Projects = React.lazy(() => import('../pages/projects'));
 const Gallery = React.lazy(() => import('../pages/gallery'));
-const Blog = React.lazy(() => import('../pages/blog'));
-const SingleBlog = React.lazy(() => import('../pages/single-blog'));
 
 
 
@@ -29,18 +26,6 @@ export const router = createBrowserRouter([
             {
                 path: "/about",
                 element: <About />
-            },
-            {
-                path: "/blog",
-                element: <Blog />
-            },
-            {
-                path: "/single-project",
-                element: <SingleProject />
-            },
-            {
-                path: "/single-blog",
-                element: <SingleBlog />
             },
             {
                 path: "/projects",
